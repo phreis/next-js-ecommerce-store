e151eec6bbb62fa6bb81684362c3f94d7dcf45ba
phreis <philippr01@yahoo.de>
2023-10-16 15:54:25 +0200

    Refine CSS

--- app/cart/Cart.tsx
+++ app/cart/Cart.tsx
@@ -17,36 +17,57 @@ export default async function Cart() {
         <form id="form" action={cartFormAction}>
           {cartProducts.map((item) => (
             <div
-              className={styles.cartContainer}
+              className={styles.cartItem}
               data-test-id={`cart-product-${item.product?.id}`}
               key={`product-${item.product?.id}`}
             >
-              <input
-                type="number"
-                name={`quantity-id-${item.product?.id}`}
-                defaultValue={item.quantity}
-                min="1"
-              />
-              <button name="changeQuantity" value="" hidden={true}>
-                Change quantity
-              </button>
-              pieces of {item.product?.name} @ {item.product?.price} each.
-              Total:
-              {item.product?.price ? item.quantity * item.product.price : 0}
-              <button name="removeItem" value={item.product?.id}>
-                Remove
-              </button>
+              {item.product?.image ? (
+                <img
+                  data-test-id="product-image"
+                  src={item.product.image}
+                  width={100}
+                  height={100}
+                  alt={item.product.name}
+                />
+              ) : (
+                ''
+              )}
+              <div className={styles.midBlock}>
+                <span>{item.product?.name}</span>
+                <span className={styles.price}>{item.product?.price} €</span>
+                <span className={styles.midBlockBottomLeft}>
+                  <label htmlFor={`quantity-id-${item.product?.id}`}>
+                    Quantity:{' '}
+                  </label>
+                  <input
+                    className={styles.quantityInput}
+                    type="number"
+                    name={`quantity-id-${item.product?.id}`}
+                    defaultValue={item.quantity}
+                    min="1"
+                  />
+                  <button name="changeQuantity" value="" hidden={true}>
+                    Change quantity
+                  </button>
+                </span>
+              </div>
+              <span className={styles.rightBlock}>
+                <button name="removeItem" value={item.product?.id}>
+                  <img
+                    src="/images/icon-trashcan.png"
+                    width={30}
+                    height={30}
+                    alt="Delete"
+                  />
+                </button>
+                Total:{' '}
+                {item.product?.price ? item.quantity * item.product.price : 0} €
+              </span>
             </div>
           ))}
         </form>
-        <div data-test-id="cart-total">Subtotal: {subTotal}</div>
-        <div>
-          <Link data-test-id="cart-checkout" role="button" href="/checkout">
-            Checkout
-          </Link>
-          <Link role="button" href="/products">
-            Continue shopping
-          </Link>
+        <div className={styles.subtotal} data-test-id="cart-total">
+          Subtotal: {subTotal} €
         </div>
       </div>
     );
--- app/cart/page.jsx
+++ app/cart/page.jsx
@@ -1,11 +1,23 @@
+import Link from 'next/link';
 import React from 'react';
 import Cart from './Cart';
+import styles from './Cart.module.scss';
 
 function CartPage() {
   return (
     <div>
       <h1>Shopping cart</h1>
-      <Cart />
+      <div className={styles.cartContainer}>
+        <Cart />
+      </div>
+      <div>
+        <Link data-test-id="cart-checkout" role="button" href="/checkout">
+          Checkout
+        </Link>
+        <Link role="button" href="/products">
+          Continue shopping
+        </Link>
+      </div>
     </div>
   );
 }
